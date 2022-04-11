33062eddc3404ac80b6a52705a00336437e3f156
kids9702 <kamji9702@naver.com>
2022-04-11 17:03:55 +0900

    0411

--- APP0411/app/src/main/java/com/example/app0411/MemberListActivity.java
+++ APP0411/app/src/main/java/com/example/app0411/MemberListActivity.java
@@ -1,6 +1,9 @@
 package com.example.app0411;
 
 import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -14,38 +17,33 @@ import org.w3c.dom.Text;
 
 public class MemberListActivity extends AppCompatActivity {
 
-    Intent inform;
-    private final int REQ_CODE_MES = 87;
-    private ActivityResult data;
     TextView newTXT;
+    private ActivityResultLauncher<Intent> mLauncher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_member_list);
-        inform = this.getIntent();
         newTXT = (TextView) findViewById(R.id.newTXT);
+        mLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>(){
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                if(result.getResultCode() == RESULT_OK){
+                    Intent dataINT = result.getData();
+                    String name = dataINT.getStringExtra("name");
+                    String email = dataINT.getStringExtra("email");
+                    String id = dataINT.getStringExtra("id");
+                    String pwd = dataINT.getStringExtra("pwd");
+                    newTXT.setText("새로 가입한 " + name +"(" + id +  ") 님 반갑습니다.");
+                }else if(result.getResultCode() == RESULT_CANCELED){
+                    Log.i("sign", "유감...");
+                }
+            }
+        });
     }
 
     public void join(View view) {
-        Intent sign_in = new Intent(this, MemberJoinActivity.class);
-        startActivityForResult(sign_in, REQ_CODE_MES);
+        mLauncher.launch(new Intent(this, MemberJoinActivity.class));
     }
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        switch(requestCode){
-            case REQ_CODE_MES:
-                if(resultCode == RESULT_OK){
-                    String name = data.getStringExtra("name");
-                    String email = data.getStringExtra("email");
-                    String id = data.getStringExtra("id");
-                    String pwd = data.getStringExtra("pwd");
-                    newTXT.setText("새로 가입한 " + name + " 님 반갑습니다.");
-                }
-                else {
-                    Log.i("sign", "유감...");
-                }
-        }
-    }
+
 }
